3e9107670b384ec99001846abb625d89884bbf17
Sunday Padua <paduasunday@gmail.com>
2024-06-17 22:53:46 +0800

    Fix Login and Register

--- alumniportal/mainContent/views.py
+++ alumniportal/mainContent/views.py
@@ -4,7 +4,8 @@ from .forms import FormWithCaptcha
 # Create your views here.
 def login(request):
     return render(request, 'login.html')
-
+def register(request):
+    return render(request, 'register.html')
 def index(request):
     context = {'form' : FormWithCaptcha()}
     return render(request, 'index.html', context)
